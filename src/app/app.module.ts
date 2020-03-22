6336ff27d5ab138d393b16cbb319389de7364dcb
Joel <joconno07@gmail.com>
2020-03-22 13:12:59 -0600

    Update to practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './Server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningOneComponent } from './warning-one/warning-one.component';
+import { WarningTwoComponent } from './warning-two/warning-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningOneComponent,
+    WarningTwoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-two/warning-two.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-two',
+  templateUrl: './warning-two.component.html',
+  styleUrls: ['./warning-two.component.css']
+})
+export class WarningTwoComponent {
+
+}
