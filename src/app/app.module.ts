297f51f4f4f7839e3bf1a9bcd52f690109444179
Joel <joconno07@gmail.com>
2020-03-22 13:40:21 -0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './Server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningOneComponent } from './warning-one/warning-one.component';
-import { WarningTwoComponent } from './warning-two/warning-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningOneComponent,
-    WarningTwoComponent
   ],
   imports: [
     BrowserModule,
